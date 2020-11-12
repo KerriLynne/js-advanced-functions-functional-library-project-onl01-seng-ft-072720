0de49de2c32d9ee80a0c65644ef6e83595d30ab3
KerriLynne <kerri.l.ogorman@gmail.com>
2020-11-12 15:41:50 -0500

    commit tree b989e5f218c33d593d39c1bc1040d2cae339bc94

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, iteratee) {
+      const newCollection = (collection instanceof Array) ? collection.slice() : Object.values(collection)
 
+      for (let i = 0; i < newCollection.length; i++)
+        iteratee(newCollection[i])
+
+        return collection
     },
 
-    map: function() {
+    map: function(collection, iteratee) {
+      if (!(collection instanceof Array))
+      collection = Object.values(collection)
 
+      const newArr = []
+
+      for (let i = 0; i < collection.length; i++)
+      newArr.push(iteratee(collection[i]))
+
+      return newArr
     },
 
-    reduce: function() {
+    reduce: function(c = [], callback = () => {}, acc) {
+			let collection = c.slice(0)
 
+			if (!acc) {
+				acc = collection[0]
+				collection = collection.slice(1)
+			}
+
+			let len = collection.length;
+
+			for (let i = 0; i < len; i++) {
+				acc = callback(acc, collection[i], collection)
+			}
+			return acc;
     },
 
     functions: function() {
